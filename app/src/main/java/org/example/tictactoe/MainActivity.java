9ae137b724b3e702e12214542ff21b563a4dcce2
dailey.yet@gmail.com <dailey.yet@gmail.com>
2015-08-17 22:33:56 +0800

    game mostly

--- app/src/main/java/org/example/tictactoe/MainActivity.java
+++ app/src/main/java/org/example/tictactoe/MainActivity.java
@@ -1,17 +1,22 @@
+/***
+ * Excerpted from "Hello, Android",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/eband4 for more book information.
+***/
 package org.example.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
 
+public class MainActivity extends Activity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+   }
 }
